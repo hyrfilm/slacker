e223d916a8e8c04c56a59bedbe04f559990c8f11
jonas <jonas.holmer@gmail.com>
2019-11-17 22:05:19 +0100

    Fixed some minor issues to allow the server to be used with some common clients

--- lib/slacker/client.ex
+++ lib/slacker/client.ex
@@ -17,33 +17,38 @@ defmodule Client do
     # find the source nick
     {:ok, src_nick} = UserService.find_nick(src_pid)
     %{socket: socket} = state
-    :ok = :gen_tcp.send(socket, ":#{src_nick} PRIVMSG :#{dst} :#{text}")
+    :ok = :gen_tcp.send(socket, ":#{src_nick} PRIVMSG #{dst} :#{text}\r\n")
+    Logger.info("Delivered message #{src_nick} -> #{dst}")
     {:noreply, state}
   end
 
-  def handle_info({:tcp, _socket, data}, state) do
+  def handle_info({:tcp, socket, data}, state) do
     [command, args] = data |> String.trim("\r\n") |> Str.pop_left
-    _response = handle_command(command, args)
+    _response = handle_command(socket, command, args)
     {:noreply, state}
   end
   def handle_info({:tcp_closed, _}, state), do: {:stop, :normal, state}
   def handle_info({:tcp_error, _}, state), do: {:stop, :normal, state}
 
-  def handle_command(command, args) do
+  def handle_command(socket, command, args) do
     case command do
-      "NICK" -> nick(args)
+      "NICK" -> nick(socket, args)
       "PRIVMSG" -> priv_msg(args)
       _ -> "?"
     end
   end
 
-  def nick(args) do
+  def nick(socket, args) do
     [nick, ""] = Str.pop_left(args)
     UserService.login(self(), nick)
+    :gen_tcp.send(socket, ":slacker 001 #{nick} :Welcome to slacker v0.01 #{nick}\r\n")
+    :gen_tcp.send(socket, ":slacker 376 #{nick} :End of MOTD command.\r\n")
     {:ok}
   end
 
   def priv_msg(args) do
+    Logger.info("Got privmsg #{args}")
+
     # unpack destination & text
     [dst_nick, text] = Str.pop_left(args)
     # find the destination pid
